dd228e9ca48ffed0d9204ac59a0041b97388460e
GitHub Action <action@github.com>
2020-09-24 11:43:05 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/rishabhrpg/cloudformationtemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rishabhrpg.cloudformationtemplate.services
+
+import com.github.rishabhrpg.cloudformationtemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
